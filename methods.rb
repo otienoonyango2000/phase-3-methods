556be3f0cb7dcd3b4abde4b2144a16c81daebd8c
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-02-07 18:33:13 +0300

    halve first commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!" 
+end 
+
+def greet name
+    puts "Hello, #{name}!"
+end    
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end    
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def halve number
+    if number.class != Integer
+        return nil
+    end
+   number/2 
+end    
+
+greet_programmer
+greet "Naureen"
+greet_with_default ("Jimmy")
+add(2,7)
+halve (6)
